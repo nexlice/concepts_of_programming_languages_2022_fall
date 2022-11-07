4dffdc866b3c07b1fe82fa9293351aedde71bb57
nexlice <auspiciouswho47@gmail.com>
2022-11-07 11:22:58 +0900

    loaded assets

---
+++ constant.py
@@ -0,0 +1,18 @@
+# character classes
+LETTER = 0
+DIGIT = 1
+UNKNOWN = 99
+EOF = 100
+
+# token codes
+INT_LIT = 10
+IDENT = 11
+SEMICOLON = 12
+ASSIGN_OP = 20
+ADD_OP = 21
+SUB_OP = 22
+MULT_OP = 23
+DIV_OP = 24
+LEFT_PAREN = 25
+RIGHT_PAREN = 26
+
